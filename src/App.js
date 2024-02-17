f0b71beadac46fd874fec0b796a9409448cf5174
Chí Cường GIáp <giapchicuong@gmail.com>
2024-02-17 11:42:40 +0700

    test

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ const App = () => {
 
   return (
     <div className="">
-      <div className="text-3xl font-bold underline">test merge branch main to giapchicuong</div>
+      <div className="text-3xl font-bold underline">test merge branch main to</div>
     </div>
   );
 };
