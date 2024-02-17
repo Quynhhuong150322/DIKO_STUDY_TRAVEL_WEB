81f1e1f9d3c280cc8e667c320d4dfd354b3258a2
Quỳnh Hương <quynhhuong@192.168.100.100>
2024-02-17 11:07:47 +0700

    moi

--- .history/src/App_20240217110337.js
+++ .history/src/App_20240217110337.js
@@ -3,7 +3,7 @@ import React from "react";
 const App = () => {
   return (
     <div className="">
-      <div className="text-3xl font-bold underline">Hương </div>
+      <div className="text-3xl font-bold underline">Hello world!</div>
     </div>
   );
 };
