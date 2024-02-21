9a631199fdf89486f305e180127e767ffdb89bad
Chí Cường GIáp <giapchicuong@gmail.com>
2024-02-21 17:27:12 +0700

    update first

--- src/App.js
+++ src/App.js
@@ -3,9 +3,7 @@ import React from "react";
 const App = () => {
   return (
     <div className="">
-
-      <div className="text-3xl font-bold underline">Nguyen Thi Quynh huong 1234</div>
-
+      <div className=""></div>
     </div>
   );
 };
