00c8b5cbe61a9cf00ad37a04c117eaf439f3c5dd
Chí Cường GIáp <giapchicuong@gmail.com>
2024-02-22 10:38:15 +0700

    install react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from "react";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import AppRoutes from "./routes/AppRoutes";
+import { BrowserRouter as Router } from "react-router-dom";
 
 const App = () => {
   return (
-    <div className="">
-      <div className=""></div>
-    </div>
+    <Router>
+      <div className="max-container">
+        <div className="">
+          <Navbar />
+        </div>
+        <div>
+          <AppRoutes />
+        </div>
+        <div>
+          <Footer />
+        </div>
+      </div>
+    </Router>
   );
 };
 
