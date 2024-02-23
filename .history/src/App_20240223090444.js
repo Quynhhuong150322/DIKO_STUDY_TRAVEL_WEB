c6682e872395800111800be11663063b11ef150e
Quỳnh Hương <quynhhuong@192.168.1.43>
2024-02-23 09:15:59 +0700

    update Langdingpage

---
+++ .history/src/App_20240223090444.js
@@ -0,0 +1,29 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import AppRoutes from "./routes/AppRoutes";
+import { BrowserRouter as Router } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import AppRoutes from "./routes/AppRoutes";
+import { BrowserRouter as Router } from "react-router-dom";
+
+const App = () => {
+  return (
+    <Router>
+      <div className="">
+        <div className="max-container">
+          <Navbar />
+        </div>
+        <div>
+          <AppRoutes />
+        </div>
+        <div className="max-container">
+          <Footer />
+        </div>
+      </div>
+    </Router>
+  );
+};
+
+export default App;
