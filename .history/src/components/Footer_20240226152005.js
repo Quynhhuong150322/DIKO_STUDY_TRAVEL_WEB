40493bd4b2029f661b383e708e1a45960dc17181
Quỳnh Hương <quynhhuong@192.168.100.102>
2024-02-26 15:30:53 +0700

    Update footer

--- .history/src/components/Footer_20240226152005.js
+++ .history/src/components/Footer_20240226152005.js
@@ -53,6 +53,83 @@ const Footer = ({ Links, title }) => {
           <MenuFooterList Name={HeadquartersList} title="Travel Blogs" />
           <MenuFooterList Name={ActivitiesList} title="Activities" />
           <MenuFooterList Name={ServiceList} title="Service" />
+
+
+          <div className="py-8 px-4">
+            <h1 className="sm:text-xl text-xl font-bold sm:text-left text-justify mb-3">
+              We Offer
+            </h1>
+            <ul className="flex flex-col gap-3">
+              {AboutUsList.map((link) => (
+                <li
+                  className="cursor-pointer hover:text-primary hover:translate-x-1 duration-300 "
+                  key={link.id}
+                >
+                  <span>{link.name}</span>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className="py-8 px-4">
+            <h1 className="sm:text-xl text-xl font-bold sm:text-left text-justify mb-3">
+              Headquarters
+            </h1>
+            <ul className="flex flex-col gap-3">
+              {AboutUsList.map((link) => (
+                <li
+                  className="cursor-pointer hover:text-primary hover:translate-x-1 duration-300 "
+                  key={link.id}
+                >
+                  <span>{link.name}</span>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className="py-8 px-4">
+            <h1 className="sm:text-xl text-xl font-bold sm:text-left text-justify mb-3">
+              Travel Blogs
+            </h1>
+            <ul className="flex flex-col gap-3">
+              {AboutUsList.map((link) => (
+                <li
+                  className="cursor-pointer hover:text-primary hover:translate-x-1 duration-300 "
+                  key={link.id}
+                >
+                  <span>{link.name}</span>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className="py-8 px-4">
+            <h1 className="sm:text-xl text-xl font-bold sm:text-left text-justify mb-3">
+              Activities
+            </h1>
+            <ul className="flex flex-col gap-3">
+              {AboutUsList.map((link) => (
+                <li
+                  className="cursor-pointer hover:text-primary hover:translate-x-1 duration-300 "
+                  key={link.id}
+                >
+                  <span>{link.name}</span>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className="py-8 px-4">
+            <h1 className="sm:text-xl text-xl font-bold sm:text-left text-justify mb-3">
+              Service
+            </h1>
+            <ul className="flex flex-col gap-3">
+              {AboutUsList.map((link) => (
+                <li
+                  className="cursor-pointer hover:text-primary hover:translate-x-1 duration-300 "
+                  key={link.id}
+                >
+                  <span>{link.name}</span>
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
         {/* footer logo  */}
         <div>
---
+++ .history/src/dummyData_20240226140703.js
@@ -0,0 +1,26 @@
+export const MenuData = [
+    {
+        id: 1,
+        name: 'Trip',
+    },
+    {
+        id: 2,
+        name: '%Deals',
+    },
+    {
+        id: 3,
+        name: 'Hotel',
+    },
+    {
+        id: 4,
+        name: 'Flight',
+    },
+    {
+        id: 5,
+        name: 'Apartment',
+    },
+    {
+        id: 6,
+        name: 'Camper',
+    },
+]
